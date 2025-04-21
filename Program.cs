10601f3f8bc4be0f3dfd34d4ec3d1ef029fcd6fa
Jin <wlsdndnjs12@naver.com>
2025-04-21 14:52:57 +0900

    fix : main base1은 틀렸습니다.
    - 내가 정답입니다.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! this is Login");
+            Console.WriteLine("Hello World! 아닌데? 내가 정답인데?");
         }
     }
 }
